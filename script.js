bc8be1ef26cfaa4da6bd2a76ccba199a16c3ba8f
Guglielmo8400 <willemlust84@gmail.com>
2025-08-26 15:04:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,8 +13,14 @@ function vierdeBOOL(){
     document.getElementById('4BOOL').style.display = 'block';
 }
 
+function uitbreiding(){
+    setInvisible();
+    document.getElementById('UITB').style.display = 'block';
+}
+
 function setInvisible(){
     document.getElementById('main').style.display = 'none';
     document.getElementById('3BOOL').style.display = 'none';
     document.getElementById('4BOOL').style.display = 'none';
+    document.getElementById('UITB').style.display = 'none';
 }
